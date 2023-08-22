474c59cb3b694d446137dc1eb9176d108ec7b5ce
hamuzah <kiggunduhamuza16@gmail.com>
2023-08-23 01:48:59 +0300

    attemp3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String [] args){
+        RWOnly obj = new RWOnly();
+        //obj.name = "hamzah";
+        //if i try to access the name variable directly it will throw an error since it is accessed by everyone
+        System.out.println("Error variable is not accessed by the public since its a private member variable");
+
+        obj.setName("hamzah");
+       System.out.println(obj.getName());
+    }
   
 }
